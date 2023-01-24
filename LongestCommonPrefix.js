729d1b764576c5e1f536e4377fc59aa34b099248
Bonnie Aue <bonnie.aue@gmail.com>
2023-01-24 11:23:50 +1030

    next Questions

---
+++ LongestCommonPrefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+
+    for (let i = 0; i < strs.length; i++){
+        let word = strs[i]
+        for (let j = 0; j<= word.length; j++){
+            let letters = word.slice(0, 2)
+        }
+    }
+
+
+    let commonArray = []
+    let result = commonArray.join('')
+    return result
+    
+};
